2dbedc31db8b27c9a04f280f8395cdb588162356
Richard Berger <richard.berger@temple.edu>
2017-04-01 14:16:35 -0400

    Create Centos 7 RPM build job which only triggers on tags

---
+++ jenkins/centos_7_tags.groovy
@@ -0,0 +1,26 @@
+node {
+    stage 'Checkout'
+
+    checkout([$class: 'GitSCM', branches: [[name: '**/tags/*']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'BuildChooserSetting', buildChooser: [$class: 'AncestryBuildChooser', ancestorCommitSha1: '', maximumAgeInDays: 30]]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'lammps-jenkins', refspec: '+refs/tags/*:refs/remotes/origin/tags/*', url: 'https://github.com/lammps/lammps.git']]])
+
+    dir('lammps-packages') {
+        git url: 'https://github.com/lammps/lammps-packages.git', credentialsId: 'lammps-jenkins', branch: 'rpm-build'
+    }
+
+    def common = load 'lammps-packages/jenkins/common.groovy'
+
+    def workdir = pwd()
+
+    //env.CCACHE_DIR=workdir + '/.ccache'
+
+    common.build_rpm('rbberger/lammps-testing:centos_7')
+
+    stage 'Archive RPMs'
+    archiveArtifacts artifacts: 'rpmbuild/**/*.rpm', onlyIfSuccessful: true
+
+    sh 'mkdir -p ${LAMMPS_DOWNLOAD_RPM_DIR}/stable/centos/7'
+    sh 'find ${LAMMPS_DOWNLOAD_RPM_DIR}/stable/centos/7 -mtime +30 -exec rm {} \\;'
+    sh 'cp -R rpmbuild/RPMS/x86_64 ${LAMMPS_DOWNLOAD_RPM_DIR}/stable/centos/7'
+
+    step([$class: 'WarningsPublisher', canComputeNew: false, consoleParsers: [[parserName: 'GNU Make + GNU C Compiler (gcc)']], defaultEncoding: '', excludePattern: '', healthy: '', includePattern: '', messagesPattern: '', unHealthy: ''])
+}
--- jenkins/jobs.dsl
+++ jenkins/jobs.dsl
@@ -1,4 +1,5 @@
 folder('rpms/daily')
+folder('rpms/tags')
 
 def scripts = ['centos_6', 'centos_7', 'fedora_21', 'fedora_22', 'fedora_23', 'fedora_24', 'fedora_25', 'opensuse_13.2', 'opensuse_42.1', 'opensuse_42.2']
 
@@ -30,6 +31,41 @@ scripts.each { name ->
                     }
                 }
                 scriptPath("jenkins/${name}.groovy")
+            }
+        }
+    }
+}
+
+def scripts2 = ['centos_7']
+
+scripts2.each { name ->
+    pipelineJob("rpms/tags/${name}") {
+        triggers {
+            githubPush()
+        }
+
+        logRotator(30)
+
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            github('lammps/lammps-packages')
+                            credentials('lammps-jenkins')
+                        }
+
+                        branches('rpm-build')
+
+                        configure { gitScm ->
+                            gitScm / 'extensions' << 'hudson.plugins.git.extensions.impl.PathRestriction' {
+                              includedRegions("jenkins/${name}_tags.groovy")
+                              excludedRegions('.*')
+                          }
+                        }
+                    }
+                }
+                scriptPath("jenkins/${name}_tags.groovy")
             }
         }
     }

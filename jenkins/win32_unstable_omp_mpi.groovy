74de7141418584e5d9b43fe2cff59841441072aa
Richard Berger <richard.berger@temple.edu>
2017-08-16 01:02:44 -0400

    Add pipelines for jenkins jobs

---
+++ jenkins/win32_unstable_omp_mpi.groovy
@@ -0,0 +1,25 @@
+node {
+    def arch = 32
+    def lammps_branch = 'unstable'
+
+    stage('Checkout') {
+        dir('lammps') {
+            git branch: lammps_branch, credentialsId: 'lammps-jenkins', url: 'https://github.com/lammps/lammps.git'
+        }
+        
+        dir('lammps-packages') {
+            git branch: 'master', credentialsId: 'lammps-jenkins', url: 'https://github.com/lammps/lammps-packages.git'
+        }
+    }
+
+    def mingw_cross = load 'lammps-packages/jenkins/mingw_cross.groovy'
+
+    mingw_cross.build_installer('-b ' + arch + ' -r ' + lammps_branch + ' -t omp -p mpi')
+
+    stage('Publish') {
+        archiveArtifacts artifacts: '*.exe', fingerprint: true, onlyIfSuccessful: true
+        sh 'mkdir -p ${LAMMPS_DOWNLOAD_WINDOWS_DIR}/' + lammps_branch
+        sh 'find ${LAMMPS_DOWNLOAD_WINDOWS_DIR}/' + lammps_branch + ' -mtime +30 -exec rm {} \\;'
+        sh 'cp *.exe ${LAMMPS_DOWNLOAD_WINDOWS_DIR}/' + lammps_branch + '/'
+    }
+}

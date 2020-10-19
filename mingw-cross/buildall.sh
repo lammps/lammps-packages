8f5276bc6fbad1492b7527eee158ee29fe401dc2
Axel Kohlmeyer <akohlmey@gmail.com>
2020-10-19 12:32:01 -0400

    convenience script to build all 7 desired package variants

---
+++ mingw-cross/buildall.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $# -ne 1 ] ; then
+   echo "Usage: $0 <revision>"
+   exit 1
+fi
+
+for b in 64 32 ; do \
+    for m in no mpi ; do \
+        python3 cmake-win-on-linux.py -v yes -r unstable -p ${m} -t omp -b ${b} -a yes
+    done
+done
+
+for b in 64 ; do \
+    for m in no mpi ; do \
+        python3 cmake-win-on-linux.py -v yes -r unstable -p ${m} -t omp -b ${b} -a no
+    done
+done
+
+for b in 64 ; do \
+    for m in no ; do \
+        python3 cmake-win-on-linux.py -v yes -r unstable -p ${m} -t omp -b ${b} -a msix
+    done
+done

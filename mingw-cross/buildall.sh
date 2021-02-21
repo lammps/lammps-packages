8e23cd865f781f691cadf6d0bccdfb998945e0fc
Axel Kohlmeyer <akohlmey@gmail.com>
2021-02-21 16:46:59 -0500

    allow different branches and tags, not just unstable

--- mingw-cross/buildall.sh
+++ mingw-cross/buildall.sh
@@ -7,18 +7,18 @@ fi
 
 for b in 64 32 ; do \
     for m in no mpi ; do \
-        python3 cmake-win-on-linux.py -v yes -r unstable -p ${m} -t omp -b ${b} -a yes
+        python3 cmake-win-on-linux.py -v yes -r "$1" -p ${m} -t omp -b ${b} -a yes
     done
 done
 
 for b in 64 32 ; do \
     for m in no mpi ; do \
-        python3 cmake-win-on-linux.py -v yes -r unstable -p ${m} -t omp -b ${b} -a no
+        python3 cmake-win-on-linux.py -v yes -r "$1" -p ${m} -t omp -b ${b} -a no
     done
 done
 
 for b in 64 ; do \
     for m in no ; do \
-        python3 cmake-win-on-linux.py -v yes -r unstable -p ${m} -t omp -b ${b} -a msix
+        python3 cmake-win-on-linux.py -v yes -r "$1" -p ${m} -t omp -b ${b} -a msix
     done
 done

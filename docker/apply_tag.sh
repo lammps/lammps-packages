ce4c7172be54275b053c8a138eb5546e700d1068
Richard Berger <richard.berger@temple.edu>
2022-02-08 11:48:03 -0500

    Rename rocky8 to rockylinux8 to match buildenv name

--- docker/apply_tag.sh
+++ docker/apply_tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TAG=$1
 docker tag lammps/lammps:centos7 lammps/lammps:${TAG}_centos7_openmpi_py3
-docker tag lammps/lammps:rocky8 lammps/lammps:${TAG}_rocky8_openmpi_py3
+docker tag lammps/lammps:rockylinux8 lammps/lammps:${TAG}_rockylinux8_openmpi_py3
 docker tag lammps/lammps:ubuntu18.04 lammps/lammps:${TAG}_ubuntu18.04_openmpi_py3
 docker tag lammps/lammps:ubuntu20.04 lammps/lammps:${TAG}_ubuntu20.04_openmpi_py3

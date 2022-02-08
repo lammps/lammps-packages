08bfbb415ac85262e8957f60b8f8cea29a998b63
Richard Berger <richard.berger@temple.edu>
2022-02-08 11:27:32 -0500

    Deprecate CentOS 8, add Rocky 8

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t lammps/lammps:centos7 -f centos7_openmpi_py3/Dockerfile .
-docker build -t lammps/lammps:centos8 -f centos8_openmpi_py3/Dockerfile .
+docker build -t lammps/lammps:rocky8 -f rocky8_openmpi_py3/Dockerfile .
 docker build -t lammps/lammps:ubuntu18.04 -f ubuntu18.04_openmpi_py3/Dockerfile .
 docker build -t lammps/lammps:ubuntu20.04 -f ubuntu20.04_openmpi_py3/Dockerfile .
--- docker/push_tag.sh
+++ docker/push_tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TAG=$1
 docker push lammps/lammps:${TAG}_centos7_openmpi_py3
-docker push lammps/lammps:${TAG}_centos8_openmpi_py3
+docker push lammps/lammps:${TAG}_rocky8_openmpi_py3
 docker push lammps/lammps:${TAG}_ubuntu18.04_openmpi_py3
 docker push lammps/lammps:${TAG}_ubuntu20.04_openmpi_py3
